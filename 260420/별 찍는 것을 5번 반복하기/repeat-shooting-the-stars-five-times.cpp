0ebce78970ae73ac460664a16a61ab3b4bac0cf4
LeeJiEun <100842799+im-insanis@users.noreply.github.com>
2026-04-20 15:12:22 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void print_star()
+{
+    printf("**********");
+}
+
+int main() {
+    print_star();
+    printf("\n");
+    print_star();
+    printf("\n");
+    print_star();
+    printf("\n");
+    print_star();
+    printf("\n");
+    print_star();
+    // Please write your code here.
+    return 0;
+}
